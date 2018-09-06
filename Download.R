d671cb6d581968bd8ebd6750ab54c517c9141313
hpstats <highperformancestats@gmail.com>
2018-09-06 21:27:08 +0100

    Fixed for cases without xg data.

--- Download.R
+++ Download.R
@@ -17,13 +17,16 @@ runningtotals <- data.table(team = teamlist$V1,
 goalratio <- 0.5
 xgratio <- 0.5
 nsxgratio <- 0.5
+noxggoalratio <- 0.5
 
 goalweight <- 1
 xgweight <- 1
 nsxgweight <- 1
+noxggoalweight <- 1
 
 matchdata2 <- matchdata[, c("date", "team1", "team2", "score1", "score2", "xg1", "xg2", "nsxg1", "nsxg2")]
-matchdata2$perfscore1 <- goalweight * ifelse(matchdata2$score1 > matchdata2$score2, 1 -
+matchdata2$perfscore1 <- ifelse(is.na(matchdata2$xg1) == FALSE,
+                                goalweight * ifelse(matchdata2$score1 > matchdata2$score2, 1 -
                                                (1 - goalratio) ^ (matchdata2$score1 - matchdata2$score2),
                                              (1 - goalratio) ^ (matchdata2$score2 - matchdata2$score1)
                                              - 1) +
@@ -32,5 +35,9 @@ matchdata2$perfscore1 <- goalweight * ifelse(matchdata2$score1 > matchdata2$scor
                                            (1 - xgratio) ^ (matchdata2$xg2 - matchdata2$xg1) - 1) +
                          nsxgweight * ifelse(matchdata2$nsxg1 > matchdata2$nsxg2, 1 - 
                                                (1 - nsxgratio) ^ (matchdata2$nsxg1 - matchdata2$nsxg2),
-                                             (1 - nsxgratio) ^ (matchdata2$nsxg2 - matchdata2$nsxg1) - 1)
+                                             (1 - nsxgratio) ^ (matchdata2$nsxg2 - matchdata2$nsxg1) - 1),
+                         noxggoalweight * ifelse(matchdata2$score1 > matchdata2$score2, 1 -
+                                               (1 - noxggoalratio) ^ (matchdata2$score1 - matchdata2$score2),
+                                             (1 - noxggoalratio) ^ (matchdata2$score2 - matchdata2$score1)
+                                             - 1))
 matchdata2$perfscore2 <- - matchdata2$perfscore1
